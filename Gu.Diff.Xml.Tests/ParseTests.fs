f1a90da4ff38c0bdcb5c67ce363ba3bfe87a33a8
Johan Larsson <johan_gavle1@hotmail.com>
2015-11-14 19:51:56 +0100

    Moved parsing to real project.

---
+++ Gu.Diff.Xml.Tests/ParseTests.fs
@@ -0,0 +1,12 @@
+﻿module Gu.Diff.Xml.Tests.ParseTests
+open System.Xml.Linq
+open System
+open Xunit
+open Gu.Diff.Xml
+
+[<Fact>]
+let ``parse valid xml returns XDocAndText``() = 
+    let result = Compare.Parse.text "<Foo />"
+    match result with
+    |Compare.XDocAndXml(Xml = xml) -> Assert.Equal(xml, "<Foo />")
+    |_ -> Assert.True(false)
---
+++ Gu.Diff.Xml/Compare.fs
@@ -0,0 +1,14 @@
+﻿module public Gu.Diff.Xml.Compare
+open System
+open System.Xml.Linq
+
+type internal DiffDocument = 
+    | XDocAndXml of Document : XDocument *  Xml : string
+    | ExcpetionAndXml of Excption : Exception * Xml : string
+
+module internal Parse =
+    let text xml =
+        try XDocAndXml(XDocument.Parse xml, xml)
+        with ex -> ExcpetionAndXml (ex, xml)
+
+
