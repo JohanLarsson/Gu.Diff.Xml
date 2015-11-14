091de2bf70f6771b841735218abdb9e7cab59d9b
Johan Larsson <johan_gavle1@hotmail.com>
2015-11-14 17:07:17 +0100

    Meh

---
+++ Gu.Diff.Xml.Tests/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Gu.Diff.Xml.Tests
+
+type Class1() = 
+    member this.X = "F#"
---
+++ Gu.Diff.Xml/Diff.fs
@@ -0,0 +1,28 @@
+﻿namespace Gu.Diff.Xml
+
+module Parsing =
+    open System.Xml.Linq
+    open System
+
+    type XDocAndSource = 
+        { Document : XDocument
+          Xml : string }
+
+    type XmlExcpetionSource = 
+        { Excption : Exception
+          Xml : string }
+
+    let parse xml =
+        try
+           { XDocument.Parse xml, xml}
+        with
+            |x ->  { x , xml }
+
+module Diff =
+    let Documents first other =
+        let firstDoc = parse first
+        let otherDoc = parse first
+
+//
+//type Diff() = 
+//    member this.X = "F#"
