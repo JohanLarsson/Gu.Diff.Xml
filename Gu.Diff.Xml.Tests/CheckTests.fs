d8222b5683a4b5f3a0f8c6c3636d1a2ad0c9ab5b
Johan Larsson <johan_gavle1@hotmail.com>
2015-11-18 00:19:29 +0100

    Tests for GetDiff.forAttribute

--- Gu.Diff.Xml.Tests/CheckTests.fs
+++ Gu.Diff.Xml.Tests/CheckTests.fs
@@ -9,16 +9,55 @@ open System.Collections.Generic
 let ``check equal declarations returns empty diffs``() = 
     let d1 = XDeclaration("1.0", "utf-8", "")
     let d2 = XDeclaration("1.0", "utf-8", "")
-    let actual = Check.checkDeclaration d1 d2
+    let actual = GetDiff.forDeclaration d1 d2
     Assert.True(actual.IsNone)
 
 [<Fact>]
 let ``check not equal declarations returns diffs``() = 
     let d1 = XDeclaration("1.0", "utf-8", null)
     let d2 = XDeclaration("1.0", "utf-16", null)
-    let actual = Check.checkDeclaration d1 d2
+    let actual = GetDiff.forDeclaration d1 d2
     Assert.True(actual.IsSome)
-    let actual = actual.Value.Diffs |> Seq.cast<IPropertyDiff> |> Seq.exactlyOne
+    let actual = 
+        actual.Value.Diffs
+        |> Seq.cast<IPropertyDiff>
+        |> Seq.exactlyOne
     Assert.Equal(d1.ToString(), actual.First)
     Assert.Equal(d2.ToString(), actual.Other)
     Assert.Equal("Encoding", actual.PropertyName)
+
+[<Fact>]
+let ``check equal attributes returns empty diffs``() = 
+    let a1 = XAttribute(XName.Get("Value", "x"), 1)
+    let a2 = XAttribute(XName.Get("Value", "x"), 1)
+    let actual = GetDiff.forAttribute a1 a2
+    Assert.True(actual.IsNone)
+
+[<Fact>]
+let ``check attributes with different values returns diffs``() = 
+    let a1 = XAttribute(XName.Get("Value", "x"), 1)
+    let a2 = XAttribute(XName.Get("Value", "x"), 2)
+    let actual = GetDiff.forAttribute a1 a2
+    Assert.True(actual.IsSome)
+    let actual = 
+        actual.Value.Diffs
+        |> Seq.cast<IPropertyDiff>
+        |> Seq.exactlyOne
+    Assert.Equal(a1.ToString(), actual.First)
+    Assert.Equal(a2.ToString(), actual.Other)
+    Assert.Equal("Value", actual.PropertyName)
+
+[<Fact>]
+let ``attributes with different names returns diffs``() = 
+    let a1 = XAttribute(XName.Get("Name1", "x"), 1)
+    let a2 = XAttribute(XName.Get("Name2", "x"), 1)
+    let actual = GetDiff.forAttribute a1 a2
+    Assert.True(actual.IsSome)
+    let actual = 
+        actual.Value.Diffs
+        |> Seq.cast<IPropertyDiff>
+        |> Seq.exactlyOne
+    Assert.Equal(a1.ToString(), actual.First)
+    Assert.Equal(a2.ToString(), actual.Other)
+    Assert.Equal("Name", actual.PropertyName)
+    Assert.Equal("LocalName", actual.Diffs |>Seq.exactlyOne |> (fun x -> (x:?> IPropertyDiff).PropertyName))
--- Gu.Diff.Xml/Check.fs
+++ Gu.Diff.Xml/Check.fs
@@ -1,45 +1,46 @@
-﻿module internal Gu.Diff.Xml.Check
+﻿module internal Gu.Diff.Xml.GetDiff
 
 open System.Xml.Linq
 
-let getDiffsFor (properties: list<IPropertyEqualityComparer<'t>>) (first: 't) (other: 't) =
+module internal Properties =
+    let forDeclaration =
+        [ 
+            CreateEqualityComparer.ForProperty<XDeclaration> <@ fun x -> x.Version @>
+            CreateEqualityComparer.ForProperty<XDeclaration> <@ fun x -> x.Encoding @>
+            CreateEqualityComparer.ForProperty<XDeclaration> <@ fun x -> x.Standalone @>
+        ]
+
+    let forName =
+            [ 
+                CreateEqualityComparer.ForProperty<XName> <@ fun x -> x.NamespaceName @>
+                CreateEqualityComparer.ForProperty<XName> <@ fun x -> x.LocalName @>
+            ]
+
+    let forAttribute =
+            [ 
+                CreateEqualityComparer.ForProperty<XAttribute> <@ fun x -> x.Value @>
+            ]
+
+let forProperties (properties: list<IPropertyEqualityComparer<'t>>) (first: 't) (other: 't) =
     properties |> List.filter (fun x -> not (x.Equals(first, other)))
                |> List.map (fun x -> x.PropertyInfo)
-
-let declarationProperties =
-    [ 
-        CreateEqualityComparer.ForProperty<XDeclaration> <@ fun x -> x.Version @>
-        CreateEqualityComparer.ForProperty<XDeclaration> <@ fun x -> x.Encoding @>
-        CreateEqualityComparer.ForProperty<XDeclaration> <@ fun x -> x.Standalone @>
-    ]
              
-let checkDeclaration (first: XDeclaration) (other: XDeclaration) = 
-    getDiffsFor declarationProperties first other
+let forDeclaration (first: XDeclaration) (other: XDeclaration) = 
+    forProperties Properties.forDeclaration first other
     |> CreateDiff.forDeclaration first other
-    
-let nameProperties =
-        [ 
-            CreateEqualityComparer.ForProperty<XName> <@ fun x -> x.NamespaceName @>
-            CreateEqualityComparer.ForProperty<XName> <@ fun x -> x.LocalName @>
-        ]
 
-let attributeProperties =
-        [ 
-            CreateEqualityComparer.ForProperty<XAttribute> <@ fun x -> x.Value @>
-        ]
-
-let checkAttribute (first: XAttribute) (other: XAttribute) = 
-    let nameDiffs = getDiffsFor nameProperties first.Name other.Name
+let forAttribute (first: XAttribute) (other: XAttribute) = 
+    let nameDiffs = forProperties Properties.forName first.Name other.Name
                     |> CreateDiff.forName first other <@ fun x -> x.Name @>
                     |> Option.map (fun x -> x:> IDiffNode)
-    let propDiffs = getDiffsFor attributeProperties first other
+    let propDiffs = forProperties Properties.forAttribute first other
     CreateDiff.forAttribute first other nameDiffs propDiffs
 
  
 //let rec checkElement first other =
 //    first.   
 //
-let checkDocument (first: XDocument) (other: XDocument) =
+let forDocument (first: XDocument) (other: XDocument) =
     { new IDocumentDiff with
           member x.First = first.ToString()
           member x.Other = other.ToString()
